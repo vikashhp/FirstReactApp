08e12fe841fec7bd402d1fffd787a4abd70757fa
vikash <vickykumar89990@gmail.com>
2023-06-18 12:54:50 +0530

    added_Expense_Item

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,28 @@
+
+function ExpenseItem(){
+    return (
+        <div>
+            <div><h2>ExpenseItem</h2>
+            <div>
+              <h2>Food</h2>
+              <div>RS 10</div>
+            </div>
+
+            <div>
+              <h2>Petrol</h2>
+              <div>RS 100</div>
+            </div>
+
+            <div>
+              <h2>Movie</h2>
+              <div>RS 200</div>
+            </div>
+            
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem;
+
+
