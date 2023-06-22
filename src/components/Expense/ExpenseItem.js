83c68c3621c404b5540c5305067b198aac258449
vikash <vickykumar89990@gmail.com>
2023-06-22 13:57:40 +0530

    added_button

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -3,15 +3,20 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem=(props)=>{
-
+   
+  const clickHandler=()=>{
+    console.log('chunnu')
+  }
+  
     return (
         <Card className="expense-item">
          
          <ExpenseDate date={props.date}></ExpenseDate>
           <div className="expense-item__description">
-            <h2>{props.title}</h2>
+            <h2 className="expense-item h2">{props.title}</h2>
             <div className="expense-item__price">${props.amount}</div>
             <div>{props.location}</div>
+            <button onClick={clickHandler}>Delete Expense</button>
           </div>
             
             
